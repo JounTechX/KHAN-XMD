7b5ae767aa17e3c2059d391b9f6f4dd52151c2eb
JounTechX <jawadxkoyeb@outlook.com>
2025-01-14 17:23:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ BOT_NAME: process.env.BOT_NAME || "KHAN-MD",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "KHAN-MD",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "🕊️,🌚,🗿,🖤,❤️,🌝",
 // chose custom react emojis by yourself 
