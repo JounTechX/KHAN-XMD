85e8eb9f08ab8630519e51e2e24cf0dc8570f689
JounTechX <jawadxkoyeb@outlook.com>
2025-01-14 11:04:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,6 @@ conn.ev.on('messages.upsert', async(mek) => {
   mek = mek.messages[0]
   if (!mek.message) return
   mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-  if (mek.key.fromMe) return  
   if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_SEEN === "true"){
     await conn.readMessages([mek.key])
   }
@@ -113,7 +112,7 @@ conn.ev.on('messages.upsert', async(mek) => {
       }
     }, { statusJidList: [mek.key.participant, jawadlike] });
   }           
-if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_SEEN === "true" && mek.message){
+if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_SEEN === "true"){
 const user = mek.key.participant
 const text = `${config.AUTO_STATUS_MSG}`
 const delayMessage = 3000;
